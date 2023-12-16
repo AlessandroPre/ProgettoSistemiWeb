d6cba8e411d70499818369a4dc24319c56e3751d
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2023-12-16 17:04:06 +0100

    Update Noleggi.vue

--- frontend/src/pages/Noleggi.vue
+++ frontend/src/pages/Noleggi.vue
@@ -1,62 +1,83 @@
-<script lang="ts">
-import { defineComponent } from "vue"
-import axios from "axios"
-import { Film, Noleggi } from "../types"
+<script lang="ts"> 
+import { defineComponent } from "vue" 
+import axios from "axios" 
 
-export default defineComponent({
-  data() {
-    return {
-      filmInput: "",
-      filmNoleggiati: [] as Noleggi[],
-      suggestions: [] as Film[],
-      datiFilm: [] as Film[],
-    }
-  },
-  methods: {
-    showSuggestions() {
-      this.suggestions = this.datiFilm.filter((film) =>
-        film.titolo.toLowerCase().includes(this.filmInput.toLowerCase())
-      ).map((film) => film);
-    },
-    selectSuggestion(suggestion: Film) {
-      this.filmInput = suggestion.titolo;
-      this.suggestions = [];
-    },
-    addFilm() {
-      const film = this.filmInput.trim();
-      if (film !== "" ) {
-        const newNoleggio: Noleggi = {
-          idnoleggio: -1,
-          datanoleggio: new Date(),
-          id_film: this.datiFilm.find((film) => film.titolo === this.filmInput)?.idfilm!,
-        };
-        this.filmNoleggiati.push(newNoleggio);
-      }
-      this.filmInput = "";
-    },
-    getFilm() {
-      axios
-        .get("/api/film")
-        .then((response) => (this.datiFilm = response.data))
-    },
-  },
-  mounted() {
-    this.getFilm();
-  },
-})
+export default defineComponent({ 
+  data() { 
+    return { 
+      nomeFilm: "", 
+      nomeAutore: "", 
+      error: "", 
+      success: "", 
+      filmList: [] as string[], 
+      suggestions: [] as any[] 
+    } 
+  }, 
+  methods: { 
+    // ottieni la lista di film disponibili 
+    getFilmList() { 
+      axios.get("/api/film") 
+      .then(response => { 
+        // trasforma la lista di oggetti film in una lista di stringhe 
+        this.filmList = response.data.map((film: any) => film.titolo) 
+      }) 
+    }, 
+    // invia la richiesta di noleggio 
+    noleggia() { 
+      axios.post("/api/noleggio", {
+         nomeFilm: this.nomeFilm, nomeAutore: this.nomeAutore 
+        }) 
+        .then(response => { 
+          // mostra un messaggio di successo 
+          this.success = response.data.message 
+          // azzera i campi del form 
+          this.nomeFilm = "" 
+          this.nomeAutore = "" 
+        }) 
+        .catch(error => { 
+          // mostra un messaggio di errore 
+          this.error = error.response.data.message 
+        }) 
+      }, 
+      // filtra i suggerimenti in base all’input dell’utente 
+      getSuggestions(input: string) { 
+        const regex = new RegExp(input, "i") 
+        return this.filmList.filter(film => film.match(regex)) 
+      },
+       // gestisci il cambio di input dell’utente 
+      onInputChange(text: string) { 
+        this.nomeFilm = text 
+        this.suggestions = this.getSuggestions(text) 
+      }, 
+      // gestisci la selezione di un suggerimento 
+      onSuggestionSelected(suggestion: any) { 
+        this.nomeFilm = suggestion.item 
+      } 
+    }, 
+    mounted() { 
+      this.getFilmList() 
+    } 
+}) 
 </script>
-<template>
-  <h1>I tuoi noleggi</h1>
-  <input type="text" v-model="filmInput" placeholder="Inserisci il titolo di un film">
-  <button @click="addFilm">Aggiungi film</button>
-  <ul v-if="suggestions.length > 0">
-    <li v-for="(suggestion, index) in suggestions" :key="index">
-      {{ suggestion.titolo }}
-    </li>
-  </ul>
-  <ul v-if="datiFilm.length > 0">
-    <li v-for="(film, index) in datiFilm" :key="index">
-      {{ film.titolo }}
-    </li>
-  </ul>
+<template> 
+  <h2>Noleggio</h2> 
+  <form @submit.prevent="noleggia"> 
+  <div> 
+    <label for="nomeFilm">Nome del film</label> 
+    <input id="nomeFilm" v-model="nomeFilm" type="text" required> 
+  </div> 
+  <div> 
+    <label for="nomeAutore">Nome autore</label> 
+    <input id="nomeAutore" v-model="nomeAutore" type="text" required> 
+  </div> 
+  <div> 
+    <button type="submit">Noleggia</button> 
+  </div> 
+  <div v-if="error"> 
+    <p>{{error}}</p> 
+  </div> 
+  <div v-if="success"> 
+    <p>{{success}}</p> 
+  </div> 
+  </form> 
 </template>
