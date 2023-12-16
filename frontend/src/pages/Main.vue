7e13134117a5edb84efac40b134e8599c3ad4ef4
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2023-12-16 17:06:13 +0100

    Create Main.vue

---
+++ frontend/src/pages/Main.vue
@@ -0,0 +1,28 @@
+<script lang="ts"> 
+import { defineComponent } from "vue" 
+import Film from "./Film.vue" 
+import Noleggio from "./Noleggi.vue" 
+import { useRoute } from "vue-router"
+
+export default defineComponent({ 
+  components: { Film, Noleggio }, 
+  setup() { 
+    // ottieni il percorso corrente 
+    const route = useRoute() 
+    // restituisci il componente corrispondente al percorso 
+    const getCurrentComponent = () => { 
+      switch (route.path) { case "/main/film": return Film 
+      case "/main/noleggio": return Noleggio 
+      default: return Film 
+    } 
+  } 
+  return { getCurrentComponent } 
+} 
+}) 
+</script>
+
+<template> 
+  <div> 
+    <component :is="getCurrentComponent()"></component> 
+  </div>
+</template>
