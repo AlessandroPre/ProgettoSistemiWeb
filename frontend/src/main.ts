34883e26f6313d538410ab32777a6a3c7c7a2da7
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2023-12-16 17:07:47 +0100

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,16 +3,18 @@ import { createRouter, createWebHistory, Router } from "vue-router"
 import "./style.css"
 import App from "./App.vue"
 import Film from "./pages/Film.vue"
-import Regista from "./pages/Registi.vue"
-import Noleggio from "./pages/Noleggi.vue"
+import Login from "./pages/Login.vue"
+import Noleggi from "./pages/Noleggi.vue"
+import Main from './pages/Main.vue'
 import NotFound from "./pages/NotFound.vue"
 
 const router: Router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: Noleggio },
-    { path: "/registi", component: Regista },
-    { path: "/film", component: Film },
+    { path: "/", component: Login },
+    { path: "/main", component: Main },
+    { path: "/main/film", component: Film },
+    { path: "/main/noleggio", component: Noleggi },
     { path: "/:pathMatch(.*)*", component: NotFound }
   ]
 })
