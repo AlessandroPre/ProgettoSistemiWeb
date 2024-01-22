0dadcbde8940825d9351f746101a42cf399d06f7
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2024-01-22 09:22:11 +0100

    Update datiDB.sql

--- backend/sql/datiDB.sql
+++ backend/sql/datiDB.sql
@@ -8,34 +8,19 @@ VALUES
 
 INSERT INTO users (username, password, role)
 VALUES
-('admin', '123456', 'admin'),
-('user1', 'password1', 'user'),
-('user2', 'password2', 'user'),
-('user3', 'password3', 'user'),
-('user4', 'password4', 'user');
+('admin', '123456', 'admin');
 
-INSERT INTO film (id_regista, titolo, genere, idGestore)
-VALUES
-(1, 'Jurassic Park', 'Avventura', 1),
-(1, 'Schindler List', 'Drammatico', 2),
-(2, 'Inception', 'Fantascienza', 3),
-(2, 'The Dark Knight', 'Azione', 4),
-(3, 'Pulp Fiction', 'Crime', 5),
-(3, 'Kill Bill', 'Azione', 1),
-(4, 'The Departed', 'Crime', 2),
-(4, 'The Wolf of Wall Street', 'Commedia', 3),
-(5, 'Lost in Translation', 'Commedia', 4),
-(5, 'The Virgin Suicides', 'Drammatico', 5);
 
-INSERT INTO noleggi (datanoleggio, id_film)
+INSERT INTO film (idfilm, id_regista, titolo, genere)
 VALUES
-('2023-01-01', 1),
-('2023-01-02', 2),
-('2023-01-03', 3),
-('2023-01-04', 4),
-('2023-01-05', 5),
-('2023-01-06', 6),
-('2023-01-07', 7),
-('2023-01-08', 8),
-('2023-01-09', 9),
-('2023-01-10', 10);
+(1, 1, 'Jurassic Park', 'Avventura'),
+(2, 1, 'Schindler List', 'Drammatico'),
+(3, 2, 'Inception', 'Fantascienza'),
+(4, 2, 'The Dark Knight', 'Azione'),
+(5, 3, 'Pulp Fiction', 'Crime'),
+(6, 3, 'Kill Bill', 'Azione'),
+(7, 4, 'The Departed', 'Crime'),
+(8, 4, 'The Wolf of Wall Street', 'Commedia'),
+(9, 5, 'Lost in Translation', 'Commedia'),
+(10, 5, 'The Virgin Suicides', 'Drammatico');
+
