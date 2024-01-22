a8edb09b614f634355be103ee1658a84da74f60e
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2024-01-22 09:27:02 +0100

    Delete backend/src/routes/registi-router.ts

--- backend/src/routes/registi-router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import * as registiController from "../controllers/registi-controller"
-
-const router: Router = Router()
-
-router.get("/api/registi", registiController.allDirector)
-router.get("/api/registi/film", registiController.filmFromDirector)
-
-export default router
