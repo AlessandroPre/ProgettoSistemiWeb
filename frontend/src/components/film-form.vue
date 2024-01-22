83226860b0f5d320781d719cb395f55838b7b592
mrtn2 <146950750+mrtn2@users.noreply.github.com>
2024-01-22 09:32:42 +0100

    Update film-form.vue

--- frontend/src/components/film-form.vue
+++ frontend/src/components/film-form.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import axios from "axios"
 import { defineComponent } from "vue"
+import { Film } from "../types"
 
 export default defineComponent({
   emits: ["submit"],
@@ -10,70 +11,67 @@ export default defineComponent({
       id_regista: "",
       titolo: "",
       genere: "",
-      idGestore: "",
+      films: [] as Film[],
     }
   },
   computed: {
     canSubmit() {
-      return this.idfilm != ""
+      return this.genere !== "" && this.id_regista && this.idfilm && this.titolo
     },
   },
   methods: {
     async onSubmit() {
+      console.log(this.idfilm)
       if (!this.canSubmit) return
       await axios.post("/api/film", {
         idfilm: this.idfilm,
         id_regista: this.id_regista,
         titolo: this.titolo,
         genere: this.genere,
-        idGestore: this.idGestore,
       })
-      this.idfilm = ""
-      this.id_regista = ""
-      this.titolo = ""
-      this.genere = ""
-      this.idGestore = ""
-      this.$emit("submit")
+      console.log(this.idfilm)
+      this.idfilm = "";
+      this.id_regista = "";
+      this.titolo = "";
+      this.genere = "";
+      this.$emit("submit");
     },
   },
 })
 </script>
 
+
 <template>
   <form class="not-prose flex relative mb-10" @submit.prevent="onSubmit">
-    <input
-        type="text"
-        v-model="idfilm"
-        class="rounded-lg border-slate-200"
-        placeholder="idfilm"
-      />
-      <input
-        type="text"
-        v-model="id_regista"
-        class="rounded-lg border-slate-200"
-        placeholder="id_regista"
-      />
-      <input
-        type="text"
-        v-model="titolo"
-        class="rounded-lg border-slate-200"
-        placeholder="titolo"
-      />
-      <input
-        type="text"
-        v-model="genere"
-        class="rounded-lg border-slate-200"
-        placeholder="genere"
-      />
-      <input
-        type="text"
-        v-model="idGestore"
-        class="rounded-lg border-slate-200"
-        placeholder="idGestore"
-      />
-    <button
+    <input 
+      ref="input" 
+      v-model="idfilm" 
+      class="rounded-lg border-slate-200" 
+      placeholder="idfilm" 
+    />
+    <input 
+      ref="input" 
+      v-model="id_regista" 
+      class="rounded-lg border-slate-200" 
+      placeholder="id_regista" 
+    />
+    <input 
+      ref="input" 
+      v-model="titolo" 
+      class="rounded-lg border-slate-200" 
+      placeholder="titolo" 
+    />
+    <input 
+      ref="input" 
+      v-model="genere" 
+      class="rounded-lg border-slate-200" 
+      placeholder="genere" 
+    />
+    <button 
       type="submit"
-      class="btn !bg-blue-500 text-white disabled:!bg-slate-100 disabled:!text-slate-300 absolute bottom-5 right-5"
-      :disabled="!canSubmit">Posta</button>
+      class="btn !bg-blue-500 text-white disabled:!bg-slate-100 disabled:!text-slate-300 absolute bottom-5 right-5"  
+      :disabled="!canSubmit">
+      Aggiungi
+    </button>
   </form>
 </template>
